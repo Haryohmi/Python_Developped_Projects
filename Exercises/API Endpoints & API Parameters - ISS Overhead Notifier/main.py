a61db7b5fd8d61cd5bdbbe275321c31268e8addf
mayowa <mahyorfrancis@gmaiil.com>
2023-09-13 01:37:35 +0100

    API & SMTP (Project-iss location update)

---
+++ Exercises/API Endpoints & API Parameters - ISS Overhead Notifier/main.py
@@ -0,0 +1,34 @@
+import requests
+from datetime import datetime as dt
+
+MY_LAT = 51.5807351
+MY_LONG = -0.127750
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+#
+# print(longitude)
+# print(latitude)
+
+parameters = {
+    "lat": MY_LAT,
+    "long": MY_LONG,
+    "formatted": 0
+}
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+
+print(sunrise)
+print(sunset)
+
+time_now = dt.now()
+print(time_now.hour)
