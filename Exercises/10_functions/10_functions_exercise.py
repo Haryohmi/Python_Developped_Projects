11588d2b3258a659ae1360ecc7194c17dbc3b75d
Mayowa Francis Dada <mahyorfrancis@gmail.com>
2024-10-03 13:26:48 +0100

    updated

--- Exercises/10_functions/10_functions_exercise.py
+++ Exercises/10_functions/10_functions_exercise.py
@@ -13,3 +13,13 @@ if __name__ == '__main__':
    d1 = int(input(f"Enter value for d1:"))
    d2 = int(input(f"Enter value for d2:"))
    calculate_area(d1, d2)
+
+
+#Write a function called print_pattern that takes integer number as an argument and prints following pattern if input number is 3,
+
+   def print_pattern(n):
+    for i in range(1, n + 1):
+        answer = ""
+        for j in range(i):
+            answer = answer + "*"
+        print(answer)
